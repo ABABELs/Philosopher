13b040bf9b5b6ad9a214b785c143a00f67953595
Arthur Abel <arthurabel@Arthurs-MacBook-Pro.local>
2023-08-02 08:42:58 +0200

    ajout du status pour determiner la mort, le reste fonctionnel

--- src/utils/threads.c
+++ src/utils/threads.c
@@ -6,7 +6,7 @@
 /*   By: arthurabel <arthurabel@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 14:40:12 by aabel             #+#    #+#             */
-/*   Updated: 2023/07/20 16:34:30 by arthurabel       ###   ########.fr       */
+/*   Updated: 2023/08/02 08:39:34 by arthurabel       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,20 +35,25 @@ void	*supervisor(void *philo_pointer)
 	t_philo	*philo;
 
 	philo = (t_philo *)philo_pointer;
-	while (philo->data->dead == 0)
+	while (philo->data->dead == 0 && (philo->status == 0 || philo->status == 1))
 	{
 		pthread_mutex_lock(&philo->lock);
-		if ((get_time() >= philo->time_to_die && philo->eating == 0) || philo->data->dead == 1)
+		if (get_time() >= philo->time_to_die && philo->data->dead == 0)//&& philo->data->dead == 0
 		{
+			pthread_mutex_lock(&philo->data->lock);
 			messages(DIED, philo);
+			philo->status = 1; //ajouter 
+			pthread_mutex_unlock(&philo->data->lock);
 		}
-		if (philo->eat_cont == philo->data->meals_nb)
+		if (philo->eat_cont == philo->data->meals_nb && philo->status == 0)
 		{
 			pthread_mutex_lock(&philo->data->lock);
 			philo->data->finished++;
 			philo->eat_cont++;
 			pthread_mutex_unlock(&philo->data->lock);
 		}
+		if (philo->status == 1) //add
+			break ;
 		pthread_mutex_unlock(&philo->lock);
 	}
 	return ((void *)0);
@@ -62,7 +67,7 @@ void	*routine(void *philo_pointer)
 	philo->time_to_die = philo->data->death_time + get_time();
 	if (pthread_create(&philo->t1, NULL, &supervisor, (void *)philo))
 		return ((void *)1);
-	while (philo->data->dead == 0)
+	while (philo->data->dead == 0 && philo->status == 0)
 	{
 		eat(philo);
 		if (philo->data->meals_nb > 0)
@@ -74,6 +79,8 @@ void	*routine(void *philo_pointer)
 				pthread_mutex_unlock(&philo->data->lock);
 				break ;
 			}
+			if (philo->status == 1)
+				break ;
 		}
 	}
 	if (pthread_join(philo->t1, NULL))
